b287c79dd1701a3c79a630c43080ce5af1573be7
Desmond Egya <egyadesmond@gmail.com>
2025-09-21 20:47:37 +0100

    Remove unnecessary comments and clear authentication classes in URL schema view

--- ayta/settings.py
+++ ayta/settings.py
@@ -149,12 +149,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Default primary key field type
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Custom user model
 # Custom user model
 AUTH_USER_MODEL = "accounts.User"
 
